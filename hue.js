00e9795507478907299998ffb07c1c08fb6c7726
CactusKing101 <chalkking17@gmail.com>
2021-07-16 18:52:08 -0400

    Update hue.js

--- hue.js
+++ hue.js
@@ -16,10 +16,84 @@ client.once('ready', () => {
       }
     }
   });
+  client.user.setActivity(`${prefix}hue`);
+  client.api.applications(client.user.id).guilds(config.guild_id).commands.post({data: {
+    name: 'on',
+    description: 'Turns on the inputted lights',
+    options: [
+      {
+        name: 'bulb',
+        type: 3,
+        description: 'Number or all',
+        required: true,
+      },
+    ],
+  }});
+  client.api.applications(client.user.id).guilds(config.guild_id).commands.post({data: {
+    name: 'off',
+    description: 'Turns off the inputted lights',
+    options: [
+      {
+        name: 'bulb',
+        type: 3,
+        description: 'Number or all',
+        required: true,
+      },
+    ],
+  }});
+  client.api.applications(client.user.id).guilds(config.guild_id).commands.post({data: {
+    name: 'color',
+    description: 'Changes the color of the lights inputted',
+    options: [
+      {
+        name: 'bulb',
+        type: 3,
+        description: 'Number or all',
+        required: true,
+      },
+      {
+        name: 'hue',
+        type: 4,
+        description: '1 - 65535',
+        required: true,
+      },
+      {
+        name: 'brightness',
+        type: 4,
+        description: '0 - 255',
+        required: false,
+      },
+      {
+        name: 'saturation',
+        type: 4,
+        description: '0 - 255',
+        required: false,
+      },
+    ],
+  }});
   console.log(`Logged into ${client.user.tag}`);
 });
 
-function light(msg, on, bulb, hue = 0, bri = 255, sat = 255) {
+function reply(id, token, cont) {
+  client.api.interactions(id, token).callback.post({data: {
+    type: 4,
+    data: {
+      content: cont,
+    }
+  }});
+};
+
+function eReply(id, token, cont) {
+  client.api.interactions(id, token).callback.post({data: {
+    type: 4,
+    data: {
+      content: cont,
+      flags: 1 << 6,
+    }
+  }});
+};
+
+function light(iId, iToken, on, bulb, hue = 0, bri = 255, sat = 255) {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
   const https = require("https");
   if (lights[bulb - 1] == null) return;
@@ -32,20 +106,19 @@ function light(msg, on, bulb, hue = 0, bri = 255, sat = 255) {
 
   const req = https.request(options, response => {
     console.log(`statusCode: ${response.statusCode}`);
-    if (response.statusCode != 200) msg.channel.send('ERROR');
+  });
+
+  req.on('error', error => {
+    console.error(error);
   });
 
   if (sat != 255 || bri != 255 || hue != 0) req.write(`{"on":${on}, "sat":${sat}, "bri":${bri},"hue":${hue}}`);
   else req.write(`{"on":${on}}`);
   req.end();
-  msg.channel.send('Done');
-
-  req.on('error', error => {
-    console.error(error);
-  });
+  reply(iId, iToken, 'Done');
 };
 
-function group(msg, on, hue = 0, bri = 255, sat = 255) {
+function group(iId, iToken, on, hue = 0, bri = 255, sat = 255) {
   process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
   const https = require("https");
   const options = {
@@ -56,73 +129,66 @@ function group(msg, on, hue = 0, bri = 255, sat = 255) {
 
   const req = https.request(options, response => {
     console.log(`statusCode: ${response.statusCode}`);
-    if (response.statusCode != 200) msg.channel.send('ERROR');
+  });
+
+  req.on('error', error => {
+    console.error(error);
   });
 
   if (sat != 255 || bri != 255 || hue != 0) req.write(`{"on":${on}, "sat":${sat}, "bri":${bri},"hue":${hue}}`);
   else req.write(`{"on":${on}}`);
   req.end();
-  msg.channel.send('Done');
-
-  req.on('error', error => {
-    console.error(error);
-  });
+  reply(iId, iToken, 'Done');
 };
 
-
-client.on('message', async msg => {
-  if (!msg.content.startsWith(prefix) || msg.channel.type === 'dm') return;
-  const args = msg.content.slice(prefix.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
-
-  if (command == 'on') {
-    if (msg.member.roles.cache.get(config.role_id)) {
-      if (args[0] == 'all') {
-        group(msg, true);
+client.ws.on('INTERACTION_CREATE', interaction => {
+  console.log(interaction.data.options);
+  if (interaction.data.name == 'on') {
+    if (interaction.member.roles.cache.get(config.role_id)) {
+      if (interaction.data.options[0].value == 'all') {
+        group(interaction.id, interaction.token, true);
         return;
       } else {
-        if (isNaN(args[0])) return msg.channel.send(`Invalid arguments\n${prefix}on bulb(1-4 \|\| all)\n\`${prefix}on <1-4 \|\| all>\``);
-        if (args[0] >= 0 && args[0] <= 4) {
-          light(msg, true, Math.floor(args[0]));
-        } else return msg.channel.send(`Invalid arguments\n${prefix}on bulb(1-4 \|\| all)\n\`${prefix}on <1-4 \|\| all>\``);
+        if (isNaN(interaction.data.options[0].value)) return reply(interaction.id, interaction.token, `Invalid arguments`);
+        if (interaction.data.options[0].value >= 0 && interaction.data.options[0].value <= 4) {
+          light(interaction.id, interaction.token, true, Math.floor(interaction.data.options[1].value));
+        } else return reply(interaction.id, interaction.token, `Invalid arguments`);
       }
-    } else return msg.channel.send(`You need the \`${config.role_name}\` role to use this`);
-  } else if (command == 'off') {
-    if (msg.member.roles.cache.get(config.role_id)) {
-      if (args[0] == 'all') {
-        group(msg, false);
-        return
+    } else return eReply(interaction.id, interaction.token, `You need the \`${config.role_name}\` role to use this`);
+  } else if (interaction.data.name == 'off') {
+    if (interaction.member.roles.cache.get(config.role_id)) {
+      if (interaction.data.options[0].value == 'all') {
+        group(interaction.id, interaction.token, false);
+        return;
       } else {
-        if (isNaN(args[0])) return msg.channel.send(`Invalid arguments\n${prefix}off bulb(1-4 \|\| all)\n\`${prefix}off <1-4 \|\| all>\``);
-        if (args[0] >= 0 && args[0] <= 4) {
-          light(msg, false, Math.floor(args[0]));
-        } else return msg.channel.send(`Invalid arguments\n${prefix}off bulb(1-4 \|\| all)\n\`${prefix}off <1-4 \|\| all>\``);
+        if (isNaN(interaction.data.options[0].value)) return reply(interaction.id, interaction.token, `Invalid arguments`);
+        if (interaction.data.options[0].value >= 0 && interaction.data.options[0].value <= 4) {
+          light(interaction.id, interaction.token, false, Math.floor(interaction.data.options[0].value));
+        } else return reply(interaction.id, interaction.token, `Invalid arguments`);
       }
-    } else return msg.channel.send(`You need the \`${config.role_name}\` role to use this`);
-  } else if (command == 'color') {
-    if (msg.member.roles.cache.get(config.role_id)) {
-      if (args[0] == 'all') {
-        if (args.length == 2 && args[1] >= 0 && args[1] <= 65536) {
-          group(msg, true, args[1]);
-        } else if (args.length == 3 && args[1] >= 0 && args[1] <= 65536 && args[2] >= 0 && args[2] <= 255) {
-          group(msg, true, args[1], args[2]);
-        } else if (args.length == 4 && args[1] >= 0 && args[1] <= 65536 && args[2] >= 0 && args[2] <= 255 && args[3] >= 0 && args[3] <= 255) {
-          group(msg, true, args[1], args[2], args[3]);
-        } else msg.channel.send(`Invalid arguments\n${prefix}color all hue(1-65535) brightness(0-255) saturation(0-255)\n\`${prefix}color all <1-65535> [0-255] [0-255]\``);
-        return
+    } else return eReply(interaction.id, interaction.token, `You need the \`${config.role_name}\` role to use this`);
+  } else if (interaction.data.name == 'color') {
+    if (interaction.member.roles.cache.get(config.role_id)) {
+      if (interaction.data.options[0].value == 'all') {
+        if (interaction.data.options.length == 2 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536) {
+          group(interaction.id, interaction.token, true, interaction.data.options[1].value);
+        } else if (interaction.data.options.length == 3 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536 && interaction.data.options[2].value >= 0 && interaction.data.options[2].value <= 255) {
+          group(interaction.id, interaction.token, true, interaction.data.options[1].value, interaction.data.options[2].value);
+        } else if (interaction.data.options.length == 4 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536 && interaction.data.options[2].value >= 0 && interaction.data.options[2].value <= 255 && interaction.data.options[3].value >= 0 && interaction.data.options[3].value <= 255) {
+          group(interaction.id, interaction.token, true, interaction.data.options[1].value, interaction.data.options[2].value, interaction.data.options[3].value);
+        } else reply(interaction.id, interaction.token, `Invalid arguments`);
+        return;
       } else {
-        if (isNaN(args[0])) return msg.channel.send(`Invalid arguments\n${prefix}color bulb(all \|\| 1-4) hue(1-65535) brightness(0-255) saturation(0-255)\n\`${prefix}color <all \|\| 1-4> <1-65535> [0-255] [0-255]\``);
-        if (args.length == 2 && args[1] >= 0 && args[1] <= 65536) {
-          light(msg, true, Math.floor(args[0]), args[1]);
-        } else if (args.length == 3 && args[1] >= 0 && args[1] <= 65536 && args[2] >= 0 && args[2] <= 255) {
-          light(msg, true, Math.floor(args[0]), args[1], args[2]);
-        } else if (args.length == 4 && args[1] >= 0 && args[1] <= 65536 && args[2] >= 0 && args[2] <= 255 && args[3] >= 0 && args[3] <= 255) {
-          light(msg, true, Math.floor(args[0]), args[1], args[2], args[3]);
-        } else msg.channel.send(`Invalid arguments\n${prefix}color bulb(1-4) hue(1-65535) brightness(0-255) saturation(0-255)\n\`${prefix}color <1-4> <1-65535> [0-255] [0-255]\``);
+        if (isNaN(interaction.data.options[0].value)) return reply(interaction.id, interaction.token, `Invalid arguments`);
+        if (interaction.data.options.length == 2 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536) {
+          light(interaction.id, interaction.token, true, Math.floor(interaction.data.options[0].value), interaction.data.options[1].value);
+        } else if (interaction.data.options.length == 3 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536 && interaction.data.options[2].value >= 0 && interaction.data.options[2].value <= 255) {
+          light(interaction.id, interaction.token, true, Math.floor(interaction.data.options[0].value), interaction.data.options[1].value, interaction.data.options[2].value);
+        } else if (interaction.data.options.length == 4 && interaction.data.options[1].value >= 0 && interaction.data.options[1].value <= 65536 && interaction.data.options[2].value >= 0 && interaction.data.options[2].value <= 255 && interaction.data.options[3].value >= 0 && interaction.data.options[3].value <= 255) {
+          light(interaction.id, interaction.token, true, Math.floor(interaction.data.options[0].value), interaction.data.options[1].value, interaction.data.options[2].value, interaction.data.options[3].value);
+        } else reply(interaction.id, interaction.token, `Invalid arguments`);
       }
-    } else return msg.channel.send(`You need the \`${config.role_name}\` role to use this`);
-  } else if (command == 'hue') {
-    msg.channel.send(`The commands are:\n${prefix}color\n${prefix}on\n${prefix}off`)
+    } else return eReply(interaction.id, interaction.token, `You need the \`${config.role_name}\` role to use this`);
   }
 });
 
