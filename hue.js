92175dec636bff6b5445a6241497df8b33080ff0
CactusKing101 <chalkking17@gmail.com>
2021-07-16 18:59:40 -0400

    Update hue.js

--- hue.js
+++ hue.js
@@ -142,7 +142,7 @@ function group(iId, iToken, on, hue = 0, bri = 255, sat = 255) {
 };
 
 client.ws.on('INTERACTION_CREATE', interaction => {
-  console.log(interaction.member.roles.cache);
+  console.log(interaction.member.roles);
   try {
     if (interaction.data.name == 'on') {
       if (interaction.member.roles.cache.get(config.role_id)) {
